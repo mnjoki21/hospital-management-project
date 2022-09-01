6d63d42943c18ce096c46e771090683dbc31df53
Michael <mickeymurage@gmail.com>
2022-09-01 20:04:13 +0300

    added doctors migration

--- server/db/migrate/01_create_doctors.rb
+++ server/db/migrate/01_create_doctors.rb
@@ -1,4 +1,10 @@
 class CreateDoctors < ActiveRecord::Migration[6.1]
   def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :department
+      t.string :specialisation
+      t.integer :office_number
+      t.integer :phone_number
   end
 end
