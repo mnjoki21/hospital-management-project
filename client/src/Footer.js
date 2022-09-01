44d1ce04ded67348fcc7fd4d8ca82aeb18b011c8
fridah <nginaomari@gmail.com>
2022-09-01 19:53:54 +0300

    added contact us

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -15,6 +15,17 @@ const Footer = () => {
             <Heading>About Us</Heading>
             <FooterLink href="#">about</FooterLink>
           </Column>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <p><strong>email:</strong>tibasasa@gmail.com</p>
+            <p><strong>Tel:</strong>+254 758982500</p>
+          </Column>
+          <Column>
+            <Heading>Social Media</Heading>
+            <p>facebook</p>
+            <p>instagram</p>
+            <p>twitter</p>
+          </Column> 
         </Row>
       </Container>
     </Box>
