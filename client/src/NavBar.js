7189506e6ac005839782c6d29946da51053c4d15
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-01 23:08:17 +0300

    add nav tag

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -3,8 +3,14 @@ import React from "react";
 function NavBar() {
     return (
         <header>
-            <h3></h3>
+            <h3>TIBASASA!</h3>
+            <nav>
+                <ul>
+                    
+                </ul>
+            </nav>
         </header>
+
     )
 
 
