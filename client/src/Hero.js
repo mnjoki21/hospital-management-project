b11a955bcdce517758c38e4cc6d4aed83f6665a5
Newton Ombese <newtonombese1@gmail.com>
2022-09-02 07:49:30 +0300

    getting ready to be up to date

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -13,7 +13,7 @@ function Hero(){
                     <div className="content">
                         <span>TibaSasa</span>
                         <h2>We are concerned about your health</h2>
-                        <p>Through market-aligned skills training, we help learners build new career responsibilities with greater confidence and capability.</p>
+                        <p>Are you sick and want to be consulted quickly without delays of queueing. Reach us and book for a session with our best doctors</p>
                         <button id="enr"><a href=" " id="enrl">Book Now</a></button>
                         <button id="ex"><a href=" " id="exp">Explore</a></button>
                     </div>           
