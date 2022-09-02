9fc862b29cdac413ee793f041a0ad2e181799d39
Newton Ombese <newtonombese1@gmail.com>
2022-09-02 03:42:57 +0300

    working on my hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Footer from './Footer';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
-  
+      <Hero />
     </div>
   );
 }
---
+++ client/src/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Hero.css'
+
+function Hero(){
+    return(
+        <div>
+            <div className="landing">
+                <div>
+                    <h2>Your health is all Your Responsibility</h2>
+                    <h4>Acknowledge TibaSasa</h4>
+                    <p>We are here for You</p>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
