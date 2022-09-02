d24ba5e86ffa9cfda7256174f52483d007258452
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-02 12:23:15 +0300

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router , Route, Routes, Switch } from 'react-router-dom';
 import Footer from './Footer';
 import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/doctors' element={ <Doctors /> } />
+           <Route path='/contacts' element={<Footer />} />
+            <Route path='/about' element={<About />} />
+
+        </Routes>
+      </Router>
 
       <Hero />
 
