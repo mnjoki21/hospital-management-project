86d103a0ef251f12b8953bc79d3d14624a62c843
Newton Ombese <newtonombese1@gmail.com>
2022-09-02 04:09:46 +0300

    working out on the hero section

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -1,17 +1,24 @@
 import React from "react";
 import './Hero.css'
+import doctor from "./img/doctor-2337835_1920.jpg"
 
 function Hero(){
     return(
         <div>
-            <div className="landing">
-                <div>
-                    <h2>Your health is all Your Responsibility</h2>
-                    <h4>Acknowledge TibaSasa</h4>
-                    <p>We are here for You</p>
+            <section className="hero">
+                <div className="rowcon">
+                    <div className="img">
+                        <img src={doctor} alt=" " />
+                    </div>
+                    <div className="content">
+                        <span>TibaSasa</span>
+                        <h2>We are concerned about your health</h2>
+                        <p>Through market-aligned skills training, we help learners build new career responsibilities with greater confidence and capability.</p>
+                        <button id="enr"><a href=" " id="enrl">Book Now</a></button>
+                        <button id="ex"><a href=" " id="exp">Explore</a></button>
+                    </div>           
                 </div>
-                <div></div>
-            </div>
+            </section>
         </div>
     )
 }
