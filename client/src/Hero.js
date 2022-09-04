b68c8fde61084f650bdf01f5250b041721aab187
Newton Ombese <newtonombese1@gmail.com>
2022-09-04 10:55:09 +0300

    getting ready to pull

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -19,6 +19,9 @@ function Hero(){
                     </div>           
                 </div>
             </section>
+            <section>
+                
+            </section>
         </div>
     )
 }
