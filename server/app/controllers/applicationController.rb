f719e5848ab63e02a185ac94c14984fb2a801734
Michael-Murage <mickeymurage@gmail.com>
2022-09-04 07:06:19 +0300

    added server get requests

--- server/app/controllers/applicationController.rb
+++ server/app/controllers/applicationController.rb
@@ -1,1 +1,16 @@
 # This is where requests will be processed
+
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+  get '/doctors' do
+    doc = Doctor.all.order(:first_name)
+    doc.to_json
+  end
+
+  get '/doctors/:id' do
+    doc = Doctor.find(params[:id])
+    doc.to_json
+  end
+
+end
