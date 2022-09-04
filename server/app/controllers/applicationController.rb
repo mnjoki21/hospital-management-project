7d01be24e1f0952c2af045d506d25b91bd70c7b8
Michael-Murage <mickeymurage@gmail.com>
2022-09-04 09:01:31 +0300

    added blueprint for other server request processes

--- server/app/controllers/applicationController.rb
+++ server/app/controllers/applicationController.rb
@@ -13,4 +13,60 @@ class ApplicationController < Sinatra::Base
     doc.to_json
   end
 
+  post '/doctors' do
+  end
+
+  patch '/doctors/:id' do
+  end
+
+  put '/doctors' do
+  end
+
+  delete '/doctors/:id' do
+  end
+
+  get '/ailments' do
+    ailment = Ailment.all.order(:ailment_name)
+    ailment.to_json
+  end
+
+  get '/ailments/:id' do
+    ailment = Ailment.find(params[:id])
+    ailment.to_json
+  end
+
+  post '/ailments' do
+  end
+
+  patch '/ailments/:id' do
+  end
+
+  put '/ailments' do
+  end
+
+  delete '/ailments/:id' do
+  end
+
+  get '/patients' do
+    patient = Patient.all.order(:patient_name)
+    patient.to_json
+  end
+
+  get '/patients/:id' do
+    patient = Patient.find(params[:id])
+    patient.to_json
+  end
+
+  post '/patients' do
+  end
+
+  patch '/patients/:id' do
+  end
+
+  put '/patients' do
+  end
+
+  delete '/patients/:id' do
+  end
+
 end
