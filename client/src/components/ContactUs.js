d4e3d1e8a9babf6d714a7c8a0b6d875b23ffe1b6
Michael <mickeymurage@gmail.com>
2022-09-06 11:29:35 +0300

    class to className

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -3,16 +3,16 @@ import "../styles/ContactUs.css"
 
 function ContactUs(){
     return(
-        <div class="containerr">
-		<div class="contact-boxx">
-			<div class="lefty"></div>
-			<div class="righty">
+        <div className="containerr">
+		<div className="contact-boxx">
+			<div className="lefty"></div>
+			<div className="righty">
 				<h2>Contact Us</h2>
-				<input type="text" class="field1" placeholder="Your Name" />
-				<input type="text" class="field1" placeholder="Your Email" />
-				<input type="text" class="field1" placeholder="Phone" />
-				<textarea placeholder="Message" class="field"></textarea>
-				<button class="btn1">Send</button>
+				<input type="text" className="field1" placeholder="Your Name" />
+				<input type="text" className="field1" placeholder="Your Email" />
+				<input type="text" className="field1" placeholder="Phone" />
+				<textarea placeholder="Message" className="field"></textarea>
+				<button className="btn1">Send</button>
 			</div>
 		</div>
 	</div>
